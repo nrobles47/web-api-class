c55181028c38f047387261be52399393caf4b510
Nate Robles <nathaniel_c_robles@progressive.com>
2023-09-12 12:29:47 -0400

    Adding AutoMapper before lunch

---
+++ EmployeesSolution/EmployeesApi/MapperProfiles/EmployeeMaps.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EmployeesApi.Data;
+
+namespace EmployeesApi.MapperProfiles;
+
+public class EmployeeMaps : Profile
+{
+    public EmployeeMaps()
+    {
+        CreateMap<EmployeeEntity, EmployeeSummaryListItemResponse>()
+            .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id.ToString()));
+
+        CreateMap<EmployeeEntity, EmployeeDetailsItemResponse>()
+            .ForMember(dest => dest.Id, opts => opts.MapFrom(src => src.Id.ToString()));
+    }
+}
